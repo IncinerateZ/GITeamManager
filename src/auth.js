7b5c04782b169552b5c02f0cf1105b06cfd36b90
IncinerateZ <inferious77@gmail.com>
2020-10-26 00:42:19 +0700

    user features

--- src/auth.js
+++ src/auth.js
@@ -4,7 +4,8 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    document.getElementById("user-name").innerHTML = profile.getId();
+    document.getElementById("user-name").innerHTML = profile.getName();
     document.getElementById("user-img").innerHTML = "<img src='" + profile.getImageUrl() + "'>"
     document.getElementById("signinbtn").style.visibility = "hidden";
+    document.getElementById("user-profile").style.visibility = "visible";
 }
