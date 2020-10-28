8b5bf1c16a248844e67ca7c108cfc5360d2170f4
IncinerateZ <inferious77@gmail.com>
2020-10-29 02:12:16 +0700

    analytics

--- src/analytics.js
+++ src/analytics.js
@@ -1,10 +1,11 @@
 
 
 var a = async () => {
-  const res = await fetch('https://api.incin.tech/analytics/update?origin=gi', {
+  const res = await fetch('https://api.incin.tech/.netlify/functions/analytics/update?origin=gi', {
     method: 'GET',
     mode: 'cors',
   });
+  console.log(await res);
   const j = await res.json();
   console.log(j);
 };
