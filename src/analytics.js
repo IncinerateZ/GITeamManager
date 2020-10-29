fdd962059ca6ddd0ea70042dabe7a1a086e499fc
IncinerateZ <inferious77@gmail.com>
2020-10-29 15:04:55 +0700

    removed clogs

--- src/analytics.js
+++ src/analytics.js
@@ -1,9 +1,9 @@
 var a = async () => {
-  const res = await fetch('https://api.incin.tech/.netlify/functions/analytics/update?origin=gi', {
+  const res = await fetch('https://api.incin.tech/.netlify/functions/analytics', {
     method: 'GET',
     mode: 'cors',
   });
   const j = await res.json();
-};
+}
 
 export default a;
